99c10a41da54055ad1045ee1041bc9e82f6610bc
Ted Lee <ted_lee@cino.com.tw>
2018-01-04 18:52:36 +0800

    updated demo1.c

--- demo1.c
+++ demo1.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./include/sp_filter.h"
-#include "./include/h2d.h"
 
 #define SIZE 256
 #define TYPE_ADDR 0
@@ -16,6 +14,8 @@
 
 void input(void);
 int dispatch_cmd(unsigned char *cmd);
+void hal_putchar(int ch);
+void hal_trans_putchar(int ch);
 
 struct _param_arr
 {
@@ -36,7 +36,7 @@ struct _cmd_option1_major
 	const struct _cmd_option2_minor *option2;
 };
 
-static struct _cmd_opcode0
+struct _cmd_opcode0
 {
 	const struct _cmd_option1_major *option1;
 };
@@ -129,6 +129,11 @@ int main(void)
 		printf("%02x ",cmd[i]);
 	}
 	*/
+	printf("cmd : ");
+	len=(int)sizeof(cmd);
+	for(i=0;i<len;i++){
+		printf("%02x ",cmd[i]);
+	}
 	dispatch_cmd(cmd);
 
 	printf("\n");
@@ -145,17 +150,18 @@ int dispatch_cmd(unsigned char *cmd)
 	int i, ret,err,send_len=0, len=((cmd[5]<<8)|cmd[6])+1;
 	printf("\n----------------dispatch_cmd---------------------\n");
 	unsigned char cmd_id;
-	unsigned char *ccd;
+	unsigned char ccd[SIZE]={'\0'};
 // 	unsigned char *buf = ccd_capture_buffer_2+4,*end;
 	unsigned char *buf,*end;
 	unsigned char chksum=cmd[1]^cmd[2]^cmd[3]^cmd[4]^cmd[5]^cmd[6]; // opcode to length
 	unsigned char result_buf1[2048];
-
+	
 	/* NOTE : if *buf initial value is not declared, segmentation fault!!! ?!
 	** if, Opcode 82h 00h 03h  cmd[1]: 82h 
 	*/
-	printf("&buf : %p &ccd : %p \n",&buf,&ccd);
-	printf("buf : %p ccd : %p \n",buf,ccd);
+	buf=ccd;
+	printf("line:%d &buf : %p &ccd : %p \n",__LINE__,&buf,&ccd);
+	printf("line:%d  buf : %p  ccd : %p \n",__LINE__,buf,ccd);
 	// saved cmd[2] : 00h cmd[3] : 03h ; Length cmd[5] High byte cmd[6] Low byte
 	buf[0]=cmd[2];
 	buf[1]=cmd[3];
@@ -165,12 +171,12 @@ int dispatch_cmd(unsigned char *cmd)
 	** +4 : buf[0] buf[1] buf[2] buf[3]  ; +(len-1) : Length == Parameters Bytes 
 	*/
 	end=buf+4+len-1;
-	printf("&end : %p end : %p\n",&end,end);
+	printf("line:%d &end : %p end : %p\n",__LINE__,&end,end);
 	/*
 	** define CMD_MASK 0x3F ; cmd_id is for static const struct _cmd_opcode new_cmd[]={......};
 	*/
 	cmd_id = (cmd[1] & CMD_MASK);
-	printf("cmd_id : %d\n",cmd_id);
+	printf("line:%d cmd_id : %d\n",__LINE__,cmd_id);
 	/*
 	** if cmd_id is in new_cmd[]={......} && Opcode[0](cmd[1]) is match opcodes[cmd_id] && 
 	*/
@@ -183,6 +189,7 @@ int dispatch_cmd(unsigned char *cmd)
 				const struct _cmd_option1_major *op1_major, *op1_macro 
 		*/	
 		op1_major = op1_macro = new_cmd[cmd_id].option1;
+		printf("line:%d opcodes[cmd_id] : %02x\n",__LINE__,opcodes[cmd_id]);
 //		printf("&new_cmd[cmd_id].option1 : %p\n",&(new_cmd[cmd_id].option1)); 
 //		printf("new_cmd[cmd_id].option1->id : %d\n",new_cmd[cmd_id].option1->id);
 		/* 
@@ -198,14 +205,17 @@ check_macro:
 		while(op1_major->option2 !=NULL){  // check if option2 exists,e.g., parameter table exists 
 			if(op1_major->id == buf[0]){   // check if id == buf[0], whereas, cmd[2]
 				op2_minor = op1_major->option2; // Assign op1_major->option2 to opinter op2_minor 
-				printf("buf[0]: %d ",buf[0]);   
-				printf("op1_major->id : %d\n",op1_major->id);
+				printf("line:%d &op2_minor : %p\n",__LINE__,&op2_minor);
+				printf("line:%d buf[0]: %02x\n",__LINE__,buf[0]);   
+				printf("line:%d op1_major->id : %d\n",__LINE__,op1_major->id);
 				while(op2_minor->arr!=0){      // NOTE: check if arr exists,e.g., parameter table options exists 
+					printf("line:%d op_minor->arr != 0\n",__LINE__);
 					if (op2_minor->id == buf[1]){ // check if minor id == buf[1], whereas, cmd[3] 
 						// CHECK TYPE ,whereas, #define TYPE_MACRO 5 
+						printf("line:%d op_minor->id == buf[1] :%02x\n",__LINE__,buf[1]);
 						if(op2_minor->type == TYPE_MACRO){  
 							buf += 4; // buf +4  
-							printf("&buf +4 : %p\n",&buf);
+							printf("line:%d &buf +4 : %p\n",__LINE__,&buf);
 							// check if buf(addrs) exceeds end(addrs),e.g., CHECK PARAMETERS BYTES
 							if(buf >= end){  // if buf buf exceeds end,?!
 								err=7;       
@@ -254,7 +264,11 @@ check_macro:
 					}
 					// if(op2_minor->id == buf[1]){}
 					// else op2_minor->id != buf[1]
+					printf("line:%d op2_minor->id : %02x != buf[1] :%02x\n",__LINE__,op2_minor->id,buf[1]);
+					printf("line:%d &op2_minor :%p\n",__LINE__,&op2_minor);
+					printf("line:%d op2_minor->type : %d\n:",__LINE__,op2_minor->type);
 					op2_minor ++; //?!  struct _cmd_option2_minor opinter shift 
+					printf("line:%d &op2_minor++ :%p\n",__LINE__,&op2_minor);
 				}
 				// while (op2_minor->arr !=0) {} 
 				// else op2_minor->arr ==0
@@ -297,30 +311,35 @@ do_macro:
 						int e, l= (buf[2]<<8 | buf[3]);
 
 						if(cmd[1]==opcodes[0] || cmd[1] == 0x90 || cmd[1] == 0x91){ //Action :0x80 HTD_NAK :0x90 HTD_ACK :0x91
-//??? exec_action() ???//
-							if((e = exec_action(op2_minor, &buf[4], cmd[4])) <0 ){ // ?! LOOKUP func e=exec_action <0 ; false 
+//exec_action()  *func_ptr to func, {0,type,2 ,(void *)sys_led_ctrl} ; static in sys_led_ctrl(unsigned char *p, int len,unsigned char flags)//
+							if((buf[4]) <0 ){ // ?! LOOKUP func e=exec_action <0 ; false 
+								printf("exec_action()<0\n");
 								err = 16;
 								goto err_nak;
 							}
-							if(e==0) {// LOOKUP func e=exec_action = 0 ; true
+							if((cmd[4] & 0x02)) {// LOOKUP func e=exec_action = 0 ; true if(e==0)
 								goto no_ack;
 							}
 							goto do_finish;
 						}
 						else{
 							if(cmd[1]& 0x40){ // 0x4,5,6,7,C,D,E,F ; if cmd[1] is GET cmd
+								printf("GET cmd !!!\n");
 								int rn;
 //??? send_bytes() ???//
-								rn = send_bytes(op1_major,op2_minor, result_buf1, send_len); // ?! LOOKUP func send_bytes() 
+//								rn = send_bytes(op1_major,op2_minor, result_buf1, send_len); // ?! LOOKUP func send_bytes() ret 0; ret -1; ret n
+								
 								if(rn<0){  // send_bytes <0
 									err=21;
 									goto err_nak;
 								}
-								if(op2_minor->type == TYPE_FUNCTION){ // ?! #define TYPE_FUNCTION 6
+								if(op2_minor->type == TYPE_FUNCTION){ //  #define TYPE_FUNCTION 6
+									rn=4; // !!!!!!!!! random number !!!!!!!!! 
 									send_len +=rn;
 								}
 								else{
 									send_len += 4 + op2_minor->num;
+									printf("send_len: %d\n",send_len);
 								}
 							}
 							else{ // if cmd[1] is NOT GET cmd ; 0x4,5,6,7,C,D,E,F 
@@ -418,7 +437,7 @@ do_finish:
 	else { // if cmd[1] is NOT GET cmd
 		int active =0;
 		if ((cmd[4] & SERCMD_NO_ACKNAK) == 0){ // ?! SERCMD_NO_ACKNAK 0x02 ; Bit 2 ACK/NAK 0 = Request ; 1 = Do NOT Request
-			printf("send_ack_packet : 7e 0f 00 00 00 00 00 0f 7e\n");
+			printf("line:%d send_ack_packet : 7e 0f 00 00 00 00 00 0f 7e\n",__LINE__);
 //			send_ack_packet(cmd[4] & SERCMD_CINO_USE); // send_ack_packet; 
 		}
 		if (cmd[1] == 0x82){ // Interface SET cmd
@@ -441,7 +460,7 @@ do_finish:
 					break;
 			}
 			if(active){
-				printf("Interface set active!!!\n");
+				printf("line:%d Interface set active!!!\n",__LINE__);
 				/*
 					usleep(500*1000); // ?! LOOKUP 
 				tx_init(); // ?! LOOKUP tx_init();
@@ -452,9 +471,13 @@ do_finish:
 	}
 
 no_ack:
+	printf("line:%d if (hal_putchar == _hal_putchar)\n",__LINE__);
+	printf("line:%d NO ACK!!!\n",__LINE__);
+/*
 	if(hal_putchar == _hal_putchar) // ?! LOOKUP _hal_putchar
 		hal_putchar(-1);
 	hal_putchar = _hal_putchar;
+*/
 //??? ???//	
 //	if (lock) pthread_mutex_unlock(&send_event_mutex); //?! LOOKUP lock & pthread_mutex_unlock(&send_event_mutex)
 	return 1;
@@ -464,14 +487,30 @@ err_nak:
 //	nak_reason = err; //  LOOKUP nak_reason :  static unsigned char nak_reason; is used in static int get_nak_status (){};
 
 	if ((cmd[4] & SERCMD_NO_ACKNAK) == 0)
-		printf("send_nak_packet : 7e 0e 00 xx 00 00 00 0e 7e");
+		printf("line:%d send_nak_packet : 7e 0e 00 %02x 00 00 00 0e 7e\n",__LINE__,err);
 //		send_nak_packet(cmd[4] & SERCMD_CINO_USE, err); // ?! LOOKUP send_nak_packet()
+		printf("line:%d if (hal_putchar == _hal_putchar)\n",__LINE__);
+/*
 	if(hal_putchar == _hal_putchar)
 		hal_putchar(-1);
 	hal_putchar = _hal_putchar;
+*/
 //??? ???//
 //	if (lock) pthread_mutex_unlock(&send_event_mutex);
 
 
 	return 0;
+}
+
+
+void hal_putchar(int ch)
+{
+	unsigned char c=ch;
+	printf("%02x",c);
+}
+
+void hal_trans_putchar(int ch)
+{	
+	unsigned char c=ch;
+	printf("%02x",c);
 }
